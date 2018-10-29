1ef1d20dae8359dab7452adb67c5fa068b50f960
PrincessChalet <chaletshelton@gmail.com>
2018-10-29 16:32:21 -0500

    TODO: add attributes to signin, enable users to join groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   resources :groups
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'pages/my_groups'
