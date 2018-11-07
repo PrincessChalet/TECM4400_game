37a9f63643e98c21481026a678c80429be204c18
PrincessChalet <chaletshelton@gmail.com>
2018-11-07 15:06:26 -0600

    fixed an issue with signing out

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'pages/my_groups'
+devise_scope :user do
+  get 'users/sign_out' =>'users/sessions#destroy'
+end
+
 end
