80b117218337f9c040f27171705090755efc534c
bakeraaron <aaron.baker15@gmail.com>
2018-11-12 15:38:56 -0600

    layout and style changes for the logged in pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_195400) do
+ActiveRecord::Schema.define(version: 2018_11_07_224108) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -38,14 +38,14 @@ ActiveRecord::Schema.define(version: 2018_11_12_195400) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "username"
-    t.integer "sign_in_count"
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
