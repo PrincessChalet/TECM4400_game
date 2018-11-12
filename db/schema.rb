d17504e336439fad538818e1e8c151aa591d12d4
chris.lam.unt <chris.lam.unt@gmail.com>
2018-11-12 14:56:12 -0500

    fixed login by adding trackable columns to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_224108) do
+ActiveRecord::Schema.define(version: 2018_11_12_195400) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,11 @@ ActiveRecord::Schema.define(version: 2018_11_07_224108) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.integer "sign_in_count"
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
