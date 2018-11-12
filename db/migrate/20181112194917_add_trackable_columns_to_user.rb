5164f66fc4bd9b2e455156ccbc1db3a785627011
chris.lam.unt <chris.lam.unt@gmail.com>
2018-11-12 15:06:46 -0500

    trying to fix merge error

--- db/migrate/20181112194917_add_trackable_columns_to_user.rb
+++ db/migrate/20181112194917_add_trackable_columns_to_user.rb
@@ -5,6 +5,8 @@ class AddTrackableColumnsToUser < ActiveRecord::Migration[5.2]
       t.integer :sign_in_count
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
+      t.string :current_sign_in_ip
+      t.string :last_sign_in_ip
     end
   end
 end
