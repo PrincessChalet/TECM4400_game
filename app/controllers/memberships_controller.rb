d3762f06da59bd6310b8c8e6258104529c656b6b
PrincessChalet <chaletshelton@gmail.com>
2018-12-02 23:55:04 -0600

    edit

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,6 +1,7 @@
 class MembershipsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_membership, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!
+  
   # GET /memberships
   # GET /memberships.json
   def index
